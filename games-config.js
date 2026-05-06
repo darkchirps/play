e882bf241d81888b260afa2c07133e76cfb02c47
FanYL219 <17371297141@163.com>
2026-05-06 18:14:14 +0800

    DL9998游戏平台

---
+++ games-config.js
@@ -0,0 +1,33 @@
+/*
+  游戏配置文件。
+
+  你主要改每个游戏的 icon 和 url：
+  - icon: 游戏图标地址，可以是本地路径，例如 "./game1/favicon.8de18.ico"
+  - url: 游戏打开地址，可以是本地页面、线上链接或相对路径
+
+  可选字段：
+  - name: 游戏名称
+  - category: 分类，会自动生成筛选按钮
+  - rating: 显示评分
+  - featured: true 表示首页推荐位
+  - description: 推荐位说明文案
+*/
+window.GAME_CONFIG = [
+  {
+    name: "闯关专家",
+    category: "肉鸽",
+    icon: "./PassingExpert/favicon.8de18.ico",
+    url: "./PassingExpert/index.html",
+    rating: "4.9",
+    featured: true,
+    description: "点击即可进入本地 PassingExpert 游戏。"
+  },
+  {
+    name: "卤蛋英雄",
+    category: "割草",
+    icon: "./TheEggHero/favicon.8de18.ico",
+    url: "./TheEggHero/index.html",
+    rating: "4.7",
+    description: "高速反应和路线选择的竞速体验。"
+  }
+];
